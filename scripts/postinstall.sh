1c06eb55d006fca52ab94244e22dbe7c73c4a89b
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2018-06-12 01:10:14 +0200

    feat(md-spell-check): add spell check US and custom
     - Latest hunspell en_US dictionary
     - extension.dic/aff is for our own words
     - Install `markdown-spellcheck`
     - Post install script concats the dictionaries
     - prespellcheck links the `.spelling` from the root directory

---
+++ scripts/postinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DICTIONARY_DIRECTORY='dictionaries'
+CONCATED_NAME='all'
+
+concat_by_extension () {
+  cat $DICTIONARY_DIRECTORY/*.$1 > $CONCATED_NAME.$1
+}
+
+for extension in 'dic' 'aff'; do
+  concat_by_extension $extension
+done
